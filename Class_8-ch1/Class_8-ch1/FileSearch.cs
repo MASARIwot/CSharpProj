c552e0b6bbfb9e8023c5efb8a883d6e0bc8c3f6c
MASARIwot <sasha.tretyak@gmail.com>
2014-11-05 20:45:12 +0200

    Class_8-ch1

---
+++ Class_8-ch1/Class_8-ch1/FileSearch.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_8_ch1
+{
+    public class FileSearch
+    {
+        public static Hashtable fileSearch(string directory)
+        {
+            try
+            {
+                DirectoryInfo d = new DirectoryInfo(directory);//Assuming Test is your Folder
+                FileInfo[] Files = d.GetFiles(); //Getting All files
+                StringBuilder str = new StringBuilder();
+                if (Files.Length == 0) { return null; }
+                foreach (FileInfo file in Files)
+                {
+                    str.Append(file.Name);
+                    str.Append(" ");
+                }
+
+                return createCollectionOfFileName(str.ToString());
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw new Exception();
+            }
+
+        }/*fileSearch()*/
+
+        private static Hashtable createCollectionOfFileName(string fileName)
+        {   
+            string[] strArr = fileName.Split(' ');
+            //create
+            Hashtable resultCollection = new Hashtable();
+            for (int i = 0; i < strArr.Length - 1; i++)
+            {
+                string[] separated = strArr[i].Split('.');
+                resultCollection.Add(separated[0], separated[1]);
+            }
+            return resultCollection;
+        }/*createCollectionOfFileName*/
+
+        public static void writeInFile(Hashtable tabelForSave, string pathName)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            using (FileStream save = new FileStream(pathName, FileMode.Create))
+            {
+                formatter.Serialize(save, tabelForSave);
+            }
+
+
+
+        }
+
+
+
+    }
+}
---
+++ Class_8-ch1/Class_8-ch1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_8_ch1
+{
+    public class Program
+    {
+        public static List<DictionaryEntry> sort(Hashtable dirs)
+        {
+            var result = new List<DictionaryEntry>(dirs.Count);
+            foreach (DictionaryEntry entry in dirs)
+            {
+                result.Add(entry);
+            }
+            result.Sort(
+                (x, y) =>
+                {
+                    IComparable comparable = x.Value as IComparable;
+                    if (comparable != null)
+                    {
+                        return comparable.CompareTo(y.Value);
+                    }
+                    return 0;
+                });
+
+            return result;
+                     
+
+        }
+
+        
+
+        public static void Main(string[] args)
+        {
+            /*Part 1*/
+            Hashtable dirs = new Hashtable();
+            try
+            {
+                /*Search all file*s*/
+                dirs = FileSearch.fileSearch(@"D:\Task_2\Class_8-ch1\Class_8-ch1\bin\Debug\ForTest");
+                /*Save in File*/
+                FileSearch.writeInFile(dirs, "Result");
+                /* Sort Hashtable!*/
+                var result = sort(dirs);
+                foreach (DictionaryEntry entry in result)
+                {
+                    Console.WriteLine(entry.Key.ToString() + ":" + entry.Value.ToString());
+                }
+              
+            }
+            catch (Exception e)
+            { Console.WriteLine(e.Message); }
+
+
+            /*Part 2*/
+
+            Console.ReadKey();
+
+        }
+    }
+}
