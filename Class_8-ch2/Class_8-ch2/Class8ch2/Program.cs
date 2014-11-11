6faf8cee412668a342a41e5e1ad5d9448173bfbc
MASARIwot <sasha.tretyak@gmail.com>
2014-11-11 20:33:13 +0200

    8ch2

---
+++ Class_8-ch2/Class_8-ch2/Class8ch2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Class_8_ch2
+{
+       
+        
+    public class Program
+    {
+        /// <summary>
+        /// Serialize information in file
+        /// </summary>
+        /// <typeparam name="T"> Type Of Infirmation</typeparam>
+        /// <param name="lsitIN"> collection</param>
+        /// <param name="path">Path to file/dir</param>
+        public static void xmlSerializer<T>(UserCollection<T> lsitIN, string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(UserCollection<T>));
+            using (TextWriter writer = new StreamWriter(path))
+            {
+                serializer.Serialize(writer, lsitIN);
+            }
+        }
+        /// <summary>
+        /// Loade information fro file
+        /// </summary>
+        /// <typeparam name="T">Type of</typeparam>
+        /// <param name="path">Path to file/dir</param>
+        /// <returns>UserCollection<T> from file</returns>
+        public static UserCollection<T> xmlDeserializer<T>(string path)
+        {
+            UserCollection<T> listOUT = new UserCollection<T>();
+            XmlSerializer serializer = new XmlSerializer(typeof(UserCollection<T>));
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                listOUT = (UserCollection<T>)serializer.Deserialize(fs);
+
+            }
+            return listOUT;
+        }
+
+        /// <summary>
+        /// MAIN
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            //Create collection
+            UserCollection<int> arr = new UserCollection<int>();
+            for (int i = 0; i <10; i++)
+            {
+                arr.Add(i);
+            }
+            //Print in different way my  UserCollection
+            //1
+            Console.WriteLine("Test 1");
+            Console.WriteLine(arr.ToStringElement());
+            //2
+            Console.WriteLine("Test 2");
+            foreach (int element in arr)
+            {
+                Console.WriteLine(element);
+            }
+            //3 NEXT!
+            Console.WriteLine("Test 3");
+            while (arr.MoveNext() == true) 
+            {
+                int element = arr.Current;
+                Console.WriteLine(element);
+            }
+            // 4 BACK!
+            Console.WriteLine("Test 4");
+            while (arr.MoveBack() == true)
+            {
+                int element = arr.Current;
+                Console.WriteLine(element);
+            }
+            //Save information in file
+            Program.xmlSerializer(arr, "SameTest.xml");
+            //Read Information from file
+            UserCollection<int> arr2 = new UserCollection<int>();
+            arr2 = Program.xmlDeserializer<int>("SameTest.xml");
+            Console.WriteLine("Test Fro Resd AND Write in file");
+            //Prin information
+            Console.WriteLine(arr.ToStringElement());
+        }
+    }
+}
---
+++ Class_8-ch2/Class_8-ch2/Class8ch2/UserCollection.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_8_ch2
+{
+   [Serializable]
+   public class UserCollection<T> : IEnumerable<T>,IEnumerator<T> ,IDisposable
+    {
+        /*
+         * If it need I can realize this program by using 
+         * Arrays (element[]) without using collection. 
+         * When arrays add new item I will copy this arr to new with size+1...
+         */
+        private List<T> userColection = new List<T>();
+        int position = 0;
+        public T this[int index] 
+        {
+            get { return userColection[index];  }
+            set { userColection[index] = value; }
+        }
+       /// <summary>
+       /// Add element to collection
+       /// </summary>
+       /// <param name="t">element to add</param>
+        public void Add(T t) 
+        {
+            userColection.Add(t);
+            
+        }
+       /// <summary>
+       /// return size of user collection
+       /// </summary>
+       /// <returns>size of collection</returns>
+        public int Capacity() 
+        {
+            return userColection.Capacity;
+        }
+       /// <summary>
+        /// This method is Clear userColection
+       /// </summary>
+        public void Clear() 
+        {
+            this.userColection = new List<T>();
+        }
+       /// <summary>
+        ///Check collection if she contains item return true
+       /// </summary>
+       /// <param name="item">checking items </param>
+       /// <returns>true/false if collections conteins item or not</returns>
+        public bool Contains(T item)
+        {
+            return userColection.Contains(item);
+        }
+       /// <summary>
+       /// copy to new array
+       /// </summary>
+       /// <param name="list">list to copy</param>
+       /// <param name="elementFrom">elemet from what start copy</param>
+        public void CopyTo(T[] list, int elementFrom) 
+        {
+            userColection.CopyTo(list, elementFrom);
+        }
+       /// <summary>
+        /// remove item from collection
+       /// </summary>
+       /// <param name="item">item to remove</param>
+       /// <returns>result of operation true/false</returns>
+        public bool Remove(T item) 
+        {
+            return userColection.Remove(item);
+        }
+       /// <summary>
+       /// Look for item in colection
+       /// </summary>
+       /// <param name="item">item to find</param>
+       /// <returns>index of item</returns>
+       public int IndexOf(T item)
+       {
+           return userColection.IndexOf(item);
+       }
+       /// <summary>
+       /// Insert element in same position in collection
+       /// </summary>
+       /// <param name="index">index of element</param>
+       /// <param name="value">new element</param>
+       public void Insert(int index , T value) 
+       {
+           userColection.Insert(index,value);
+       }
+       /// <summary>
+       /// Remove element from colection by index
+       /// </summary>
+       /// <param name="index">index of element ot delet</param>
+       /// <returns>result of operation</returns>
+       public bool RemoveAt(int index)
+       {
+           if (index < 0) return false;
+           if (userColection.Capacity >= index) { userColection.RemoveAt(index); return true; }
+           return false;
+       }
+       /// <summary>
+       /// For printing elements with ','-> is split
+       /// </summary>
+       /// <returns>String with all items from colection</returns>
+       public string ToStringElement() 
+       {
+           string str = "[ ";
+           for (int i = 0; i < userColection.Count; i++) 
+           {
+               str += ",";
+               str += userColection[i];
+              
+           }
+           str += "]";
+           return str;
+
+       }
+
+        //IEnumerator 1
+        public bool MoveNext()
+        {
+            if (position < userColection.Count-1) 
+            {
+                position++;
+                return true;
+            }else
+            return false;
+        }
+        //get position before/ previous position
+        public bool MoveBack() 
+        {
+            if (position > 0)
+            {
+                position--;
+                return true;
+            }
+            else
+                return false;
+
+        }
+        //IEnumerator 2
+        public void Reset()
+        {
+            position = 0;
+        }
+        //IEnumerator 3
+        public T Current
+        {
+            get { return userColection[position]; }
+        }
+        //IEnumerable 1
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+        //IDisposable
+        public void Dispose()
+        {
+            ((IEnumerator)this).Reset();
+        }
+        //&&&
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+
+        object System.Collections.IEnumerator.Current
+        {
+            get { return userColection[position]; }
+        }
+
+       
+    }
+}
